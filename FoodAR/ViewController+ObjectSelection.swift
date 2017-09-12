e91650141d863ea9ae6cbc9d6c2f5f5805b9b01c
Wasupol Tungsakultong <peepo157@gmail.com>
2017-09-12 12:40:02 +0700

    Init commit

---
+++ FoodAR/ViewController+ObjectSelection.swift
@@ -0,0 +1,73 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+File info
+*/
+
+import UIKit
+import SceneKit
+
+extension ViewController: VirtualObjectSelectionViewControllerDelegate, VirtualObjectManagerDelegate {
+    
+    // MARK: - VirtualObjectManager delegate callbacks
+    
+    func virtualObjectManager(_ manager: VirtualObjectManager, willLoad object: VirtualObject) {
+        DispatchQueue.main.async {
+            // Show progress indicator
+            self.spinner = UIActivityIndicatorView()
+            self.spinner!.center = self.addObjectButton.center
+            self.spinner!.bounds.size = CGSize(width: self.addObjectButton.bounds.width - 5, height: self.addObjectButton.bounds.height - 5)
+            self.addObjectButton.setImage(#imageLiteral(resourceName: "buttonring"), for: [])
+            self.sceneView.addSubview(self.spinner!)
+            self.spinner!.startAnimating()
+            
+            self.isLoadingObject = true
+        }
+    }
+    
+    func virtualObjectManager(_ manager: VirtualObjectManager, didLoad object: VirtualObject) {
+        DispatchQueue.main.async {
+            self.isLoadingObject = false
+            
+            // Remove progress indicator
+            self.spinner?.removeFromSuperview()
+            self.addObjectButton.setImage(#imageLiteral(resourceName: "ic_room_service"), for: [])
+            self.addObjectButton.setImage(#imageLiteral(resourceName: "ic_room_service"), for: [.highlighted])
+            self.textManager.showMessage("\(object.definition.displayName) is price \n \(object.definition.price)", autoHide: false)
+        }
+    }
+    
+    func virtualObjectManager(_ manager: VirtualObjectManager, transformDidChangeFor object: VirtualObject) {
+        DispatchQueue.main.async {
+            self.textManager.showMessage("\(object.definition.displayName) is price \n \(object.definition.price)", autoHide: false)
+        }
+    }
+    
+    func virtualObjectManager(_ manager: VirtualObjectManager, couldNotPlace object: VirtualObject) {
+        textManager.showMessage("CANNOT PLACE OBJECT\nTry moving left or right.")
+    }
+    
+    // MARK: - VirtualObjectSelectionViewControllerDelegate
+    
+    func virtualObjectSelectionViewController(_: VirtualObjectSelectionViewController, didSelectObjectAt index: Int) {
+        guard let cameraTransform = session.currentFrame?.camera.transform else {
+            return
+        }
+        
+        let definition = VirtualObjectManager.availableObjects[index]
+        let object = VirtualObject(definition: definition)
+        let position = focusSquare?.lastPosition ?? float3(0)
+        virtualObjectManager.loadVirtualObject(object, to: position, cameraTransform: cameraTransform)
+        if object.parent == nil {
+            serialQueue.async {
+                self.sceneView.scene.rootNode.addChildNode(object)
+            }
+        }
+    }
+    
+    func virtualObjectSelectionViewController(_: VirtualObjectSelectionViewController, didDeselectObjectAt index: Int) {
+        virtualObjectManager.removeVirtualObject(at: index)
+    }
+    
+}
